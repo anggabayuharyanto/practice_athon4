3c12f55943f22c93b91aa961eaabb416c892c6d8
Angga Bayu Haryanto <anggaheartsound@gmail.com>
2023-07-28 21:56:51 +0700

    Brief description of changes

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js
+
+module.exports = {
+    development: {
+      username: 'root',
+      password: 'password',
+      database: 'practice_athon',
+      host: 'localhost',
+      dialect: 'mysql',
+    },
+    // You can add more environments like "test" and "production" if needed
+  };
+  
---
+++ index.js
@@ -0,0 +1,5 @@
+const fs = require('fs')
+const person = require("./create.js")
+//const sabrina = require("./create.json")
+
+console.log(person)
